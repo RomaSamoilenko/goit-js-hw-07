dca4f1e4fe1f9a03c8ad2263f996062b5ee18073
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-01-29 22:27:43 +0200

    1

--- js/task-6.js
+++ js/task-6.js
@@ -9,6 +9,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttomCreate.addEventListener("clik", () => {
+	if (Number(inputNumber.value) >= 1 && Number(inputNumber.value) <=100) {
+		createBoxes(Number(inputNumber.value));}
+	else {alert"Enter 1 and 100"}
+}
+)
+
 const createBoxes = amount => {
 	const elementsToAdd = []
 	for (let i = 0; i < amount; i++) {
@@ -26,12 +34,21 @@ const destroyBoxes = () => {
 }
 
 btnCreate.addEventListener('click', () => {
-	let boxesToAdd = createBoxes(inputNum.value)
+	let boxesToAdd = createBoxes(Number(inputNum.value))
 	boxes.append(...boxesToAdd)
 })
 
-console.log(inputNum.value)
+// console.log(inputNum.value)
 
-btnDestroy.addEventListener('click', () => {
-	destroyBoxes.call()
-})
+btnDestroy.addEventListener('click', destroyBoxes);
+
+
+btnCreate.addEventListener('click', () => {
+	const amount = Number(inputNum.value);
+	if (amount >= 1 && amount <= 100) {
+	  destroyBoxes(); // Очистити існуючі блоки перед створенням нових
+	  let boxesToAdd = createBoxes(amount); 
+	  boxes.append(...boxesToAdd);
+	  inputNum.value = ''; // Очистити значення поля вводу після створення блоків
+	}
+  });
